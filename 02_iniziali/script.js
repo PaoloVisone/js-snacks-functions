258995ba94a8a32d75810a88908faf1c1c76ff20
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-09 17:20:22 +0100

    Fix Function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,21 +4,40 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function getFirstLetter() {
+function getFirstLetter(names) {
 
+    // Creo un array
+    const firstLetter = []
+
+    // Ciclo l'array
     for (let i = 0; i < names.length; i++) {
-        let letter = names[i].slice(-1, 1);
-        return letter
+        // console.log(names[i]);
 
+        // Seleziono i nomi dell'array
+        const nome = names[i];
+
+        // console.log(nome[0]);
+
+        // Seleziono solo la prima lettera in posizione [0]
+        const letter = nome[0];
+
+        // Push delle lettere nell'nuovo array
+        firstLetter.push(letter);
     }
+    console.log(firstLetter);
+
+    // Fine ciclo 
+    return firstLetter;
 }
 
-console.log(getFirstLetter());
 
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+// console.log(getFirstLetter(names));
+const firstLetter = getFirstLetter(names);
+console.log(firstLetter);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
